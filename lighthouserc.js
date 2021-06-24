a7e5c63a90a3b06fbc275041041e5d63f486d520
Prakash Gamit <prakashgamit23@gmail.com>
2021-06-24 19:12:30 +0530

    Update lighthouse config

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,12 @@
 module.exports = {
   ci: {
+    collect: {
+      numberOfRuns: 2,
+      url: [
+        'http://localhost/index.html',
+        'http://localhost/404.html',
+      ]
+    },
     upload: {
       target: 'temporary-public-storage',
     },
